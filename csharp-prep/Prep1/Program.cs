c48b3ab75669e47273d6d13e83b20ccb4fcf6a96
vickygioviBYU <can23001.byui.edu>
2023-04-18 18:54:11 -0300

    Prepare 1 and 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        /*
+        
+        What is your first name? Scott
+        What is your last name? Burton
+
+        Your name is Burton, Scott Burton.
+
+        */
+
+        Console.WriteLine("What is your first name?");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string surname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {surname}, {name} {surname}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        /*
+        
+        Write a program that determines the letter grade for a course according to the following scale:
+
+        A >= 90
+        B >= 80
+        C >= 70
+        D >= 60
+        F < 60
+
+        */
+
+        Console.WriteLine("What is your percentage? ");
+        string percentage = Console.ReadLine();
+        int percentageInt = int.Parse(percentage);
+        string letter;
+
+        if (percentageInt >= 90) {
+            letter = "A";
+        } else if (percentageInt >= 80) {
+            letter = "B";
+        } else if (percentageInt >= 70) {
+            letter = "C";
+        } else if (percentageInt >= 60) {
+            letter = "D";
+        } else {
+            letter = "F";
+        }
+
+        int percentageRemainder = percentageInt % 10;
+        string sign;
+
+        if (percentageRemainder >= 7) {
+            sign = "+";
+        } else if (percentageRemainder < 3) {
+            sign = "-";
+        } else {
+            sign = "";
+        }
+
+        letter = letter + sign;
+
+        Console.WriteLine($"Grade: {letter}");
+
+        if (percentageInt >= 70) {
+            Console.WriteLine("You passed!!");
+        } else {
+            Console.WriteLine("Try next time");
+        }
     }
 }
