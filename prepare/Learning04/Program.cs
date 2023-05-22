138562dfd40b6abc0b8dbc26a8221bc5f1cd9264
vickygioviBYU <can23001.byui.edu>
2023-05-22 13:58:33 -0300

    Inheritance Learning

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Giovanni Canavesi", "Differential Ecuations");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment math1 = new MathAssignment("Juan Perez", "Fractions", "Section 10", "Problems 20-30");
+
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment writing1 = new WritingAssignment("Mathias Gonzalez", "Fantasy Books", "The fantasy gender along the years");
+        Console.WriteLine(writing1.GetWritingInformation());
+    
     }
 }
